23439da4af8f79f22e726a6a6d9973fbac7468cc
rashi <eynorashi@gmail.com>
2023-05-06 18:34:36 +0530

    add some files

--- src/component/Quizz.js
+++ src/component/Quizz.js
@@ -1,29 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./quiz.css";
-import questions from "./QuizzData"
+import questions from "./QuizzData";
 
 
 const Quizz = () => {
+  const[currentQuestion,setCurrentQuestion]=useState(0);
+  const[score,setScore] =useState(0);
+  const[correctAns,setCorrectAns]=useState(0);
+  const handleAnswerOption=(isCorrect)=>{
+    if(isCorrect){
+      setScore(score+5)
+      setCorrectAns(correctAns+1)
+    }
+  }
+  const handleNextOption =()=>{
+    const nextQuestion = currentQuestion+1;
+    setCurrentQuestion(nextQuestion);
+  }
   return (
     <div className='app'>
         <div className='question-section'>
-           <h5>Score:5</h5>
+           <h5>Score:{score}</h5>
            <div className='question-count'>
-            <span>Question 1 of 4</span>
+            <span>Question {currentQuestion+1} of {questions.lenght}</span>
            </div>
            <div className='question-text'>
-             {questions[0].questionText} 
-             
+             {questions[currentQuestion].questionText} 
            </div>
         </div>
         <div className='answer-section'>
-          <button>Answer</button>
+          {questions[currentQuestion].answerOption.map((ans,i)=>{
+             return <button onClick={handleAnswerOption} key={i}>{ans.answwerText}</button>
+          })}
+          
           <div className='actions'>
             <button>Quit</button>
-            <button>Next</button>
+            <button onClick={handleNextOption}>Next</button>
           </div>
         </div>
-       
     </div>
   ) 
 }
